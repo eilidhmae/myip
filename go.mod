13963ed22ef4c4e75fcd82a34a00d3b6a984cbdd
Eilidh Robey <erobey+github@sonatype.com>
2023-03-01 21:14:42 -0800

    use ipcheck to fetch ISP info from wtf

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module myip
 
 go 1.20
+
+require github.com/troby/ipcheck v0.0.0-20191027062330-94d3263aa50f
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"fmt"
+	"github.com/troby/ipcheck"
 )
 
 func main() {
@@ -11,5 +12,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%-20s\t%s\n", ip.Address, ip.Label())
+	// get ISP
+	wtf, err := ipcheck.WTFMyIP()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%-20s\t%-16s\t%s\n", ip.Address, ip.Label(), wtf.YourFuckingISP)
 }
